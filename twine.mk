0016582d215331ddcb86e8e26cc1e3e5fbc7541e
Ben Finney <ben+python@benfinney.id.au>
2022-10-22 10:51:15 +1100

    Install the development requirements prior to using Twine.

--- twine.mk
+++ twine.mk
@@ -17,12 +17,19 @@ PYTHON_TWINE_UPLOAD_OPTS ?= --sign --identity ${RELEASE_SIGNING_KEYID}
 TWINE_DIST_FILES ?= ${DIST_DIR}/*
 
 
+.PHONY: pip-install-twine-requirements
+pip-install-twine-requirements:
+	$(PYTHON) -m pip install .[devel]
+
+
 .PHONY: twine-upload
+twine-upload: pip-install-twine-requirements
 twine-upload: setuptools-dist
 	$(PYTHON_TWINE) upload ${PYTHON_TWINE_UPLOAD_OPTS} ${TWINE_DIST_FILES}
 
 
 .PHONY: twine-check
+twine-check: pip-install-twine-requirements
 twine-check: setuptools-dist
 	$(PYTHON_TWINE) check ${TWINE_DIST_FILES}
 
