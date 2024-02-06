c031a51d1650af9d04c3ed989254540f556890e5
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2024-02-06 21:35:31 +0800

    alvistack/3.0.2
        git clean -xdf
        tar zcvf ../python-daemon_3.0.2.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-daemon.spec ../python-daemon_3.0.2-1.spec
        cp ../python*-daemon*3.0.2*.{gz,xz,spec,dsc} /osc/home\:alvistack/pagure.io-python-daemon-3.0.2/
        rm -rf ../python*-daemon*3.0.2*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- setup.py
+++ setup.py
@@ -53,11 +53,11 @@ devel_requirements = [
 
 setup_kwargs = dict(
         name=metadata.distribution_name,
+        version="3.0.2",
         packages=find_packages(exclude=["test", "util"]),
         entry_points={
             "setuptools.finalize_distribution_options": [
                 "description_fields = util.packaging:derive_dist_description",
-                "version = util.packaging:derive_version",
                 "maintainer = util.packaging:derive_maintainer",
                 ],
             },
@@ -74,7 +74,7 @@ setup_kwargs = dict(
             "packaging",
             "lockfile >=0.10",
             ],
-        python_requires=">=3",
+        python_requires=">=3.7",
         extras_require={
             'test': test_requirements,
             'build': build_requirements,
